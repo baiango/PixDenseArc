8da898189270b492d74afa1130c1be10781337cc
baiango <105705580+baiango@users.noreply.github.com>
2023-11-29 09:29:21 +0800

    Added arithmetic encoding

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # PixDenseArc
-What we can achive in O(n^3)?
+What we can achieve in O(n^2)?
+
+# Citation
+Log Calculator
+> Szyk, B. and Pál, T. Log Calculator (Logarithm). Available at: https://www.omnicalculator.com/math/log. Accessed: 29 November 2023.
+
+Simple bytewise context mixing demo
+> https://encode.su/threads/541-Simple-bytewise-context-mixing-demo
+
+Lossless Data Compression via Substring Enumeration
+> Dubé, Danny and Vincent Beaudoin. “Lossless Data Compression via Substring Enumeration.” 2010 Data Compression Conference (2010): 229-238.
---
+++ entropy_encoding/arithmetic_encoding.py
@@ -0,0 +1,239 @@
+from fractions import Fraction
+from math import log
+
+
+def count_frequency(text):
+	frequency = {}
+	for letter in text:
+		if letter in frequency:
+			frequency[letter] += 1
+		else:
+			frequency[letter] = 1
+	return frequency
+
+
+def count_distribute_probability(frequency):
+	"""
+	Calculates the probability ranges for each character in a message based on
+	frequency.
+
+	This function takes a dictionary of character frequencies and converts it
+	into a dictionary of probability ranges for each character. These ranges
+	are represented as tuples of fractions, indicating the start and end of
+	each character's probability range.
+
+	Parameters:
+	frequency (dict):	A dictionary with characters as keys and their
+						frequency counts as values.
+
+	Returns:
+	dict:	A dictionary with characters as keys and tuples (start, end) as
+			values representing the probability range of each character.
+	"""
+	# Calculate the total count of all characters
+	total_count = sum(frequency.values())
+
+	# Convert frequencies to probabilities
+	char_probabilities = {char: Fraction(count, total_count) for char, count in frequency.items()}
+
+	# Initialize the dictionary for probability ranges
+	probability_ranges = {}
+
+	# Start with a cumulative probability of zero
+	previous_probability = Fraction(0)
+
+	# Calculate the cumulative probability ranges for each character
+	for char, probability in char_probabilities.items():
+		# Define the start of the current range
+		current_range_start = previous_probability
+
+		# Define the end of the current range
+		current_range_end = previous_probability + probability
+
+		# Store the range for the character
+		probability_ranges[char] = (current_range_start, current_range_end)
+
+		# Update the cumulative probability for the next character
+		previous_probability = current_range_end
+
+	return probability_ranges
+
+
+def arithmetic_encode(message, symbol_probabilities):
+	"""
+	Compresses a message into a fraction using arithmetic encoding. This
+	function uses fractions to determine the probability of each symbol in the
+	message, aiming for precision in the encoding process.
+
+	Optimal compression is often achieved with a moderate level of probability
+	precision, approximately 1 and 1/3 times the count of unique symbols.
+	Exceeding this level of precision can lead to less efficient compression
+	due to the need to quantize the probabilities or because of an increased
+	size of the final compressed fraction.
+
+	A noteworthy aspect of this method is that when the denominators of the
+	symbol probabilities are powers of 2, the logarithm base 2 of the
+	denominator can be used. This allows for a more efficient storage,
+	potentially saving the information in 6 bits instead of 8 bytes. However,
+	this is specific to cases where such a power-of-2 denominator is present.
+
+	These observations and techniques are based on a limited set of tests and
+	may not apply universally. Different data types or probability
+	distributions might yield different results.
+
+	Parameters:
+	message (str): The text message to be compressed.
+	symbol_probabilities (dict):	A dictionary where each key is a symbol
+									from the message and each value is a tuple
+									representing the probability range of that
+									symbol as fractions.
+
+	Returns:
+	tuple:	A tuple containing the compressed fraction, the logarithm (base 2)
+			of the denominator of the fraction (if it's a power of 2), and the
+			encoding process steps.
+
+	Note:
+	The effectiveness of this compression method can vary based on the input
+	data. It is recommended to experiment with different data and probability
+	distributions to fully understand the method's applicability and
+	efficiency.
+
+	Encoding example:
+	message = 'kiwi'
+	probabilities = {'k': (0, Fraction(1, 3)), 'i': (Fraction(1, 3), Fraction(2, 3)), 'w': (Fraction(2, 3), 1)}
+
+	- 'k': Probability range from 0 to 1/3
+	- 'i': Probability range from 1/3 to 2/3
+	- 'w': Probability range from 2/3 to 1
+
+	Now, let's go through the encoding process for "kiwi":
+	1.	Start with the full interval [0, 1].
+	2.	Encoding 'k':
+			The range for 'k' is [0, 1/3]. So, our interval after encoding 'k'
+			is [0, 1/3).
+	3.	Encoding 'i':
+			We split the current interval [0, 1/3] for 'i'. The 'i' falls in
+			the middle third of any interval. So, the new interval is
+			[1/9 (which is 1/3 of 0), 2/9 (which is 1/3 of 1/3)].
+	4.	Encoding 'w':
+			Now we split the interval [1/9, 2/9) for 'w'. The 'w' takes the
+			last third of any interval. So, the new interval is
+			[5/27 (which is 2/3 of 1/9), 6/27 (which is 2/3 of 2/9)].
+	5.	Encoding the final 'i':
+			Finally, we split the interval [5/27, 6/27) for 'i'. The 'i' takes
+			the middle third. So, the final interval is
+			[16/81 (which is 1/3 of 5/27), 17/81 (which is 1/3 of 6/27)].
+	6.	Choose a fraction within the final interval:
+			Any fraction within [16/81, 17/81) can represent "kiwi".
+			For simplicity, let's take the middle of this interval:
+			(16/81 + 17/81) / 2 = 33/162.
+
+	So, the string "kiwi" can be represented by the fraction 33/162 (or 11/54)
+	in this arithmetic encoding scheme.
+	"""
+	lower_bound = Fraction(0, 1)
+	upper_bound = Fraction(1, 1)
+	encoding_process = []
+
+	for char in message:
+		range_width = upper_bound - lower_bound
+		upper_bound = lower_bound + range_width * symbol_probabilities[char][1]
+		lower_bound = lower_bound + range_width * symbol_probabilities[char][0]
+		encoding_process.append((char, lower_bound, upper_bound))
+
+	fraction = (lower_bound + upper_bound) / 2
+
+	return fraction, log(fraction.denominator, 2), encoding_process
+
+
+def arithmetic_decode(encoded_number, symbol_probabilities, message_length):
+	"""
+	Encoding example:
+	1.	First Character:
+	-	The initial interval [0, 1] is divided into:
+		-	[0, 1/3] for 'k'
+		-	[1/3, 2/3] for 'i'
+		-	[2/3, 1] for 'w'
+	-	The fraction 33/162 falls in the interval [1/3, 2/3], so the first
+		character is 'i'.
+
+	2.	Second Character:
+	-	Update the interval to [1/3, 2/3] and divide it:
+		-	[1/3, 1/2] for 'k'
+		-	[1/2, 5/6] for 'i'
+		-	[5/6, 2/3] for 'w'
+	-	The fraction 33/162, when scaled to this new interval, falls in
+		[1/3, 1/2], so the second character is 'k'.
+
+	3.	Third Character:
+	-	Update the interval to [1/3, 1/2] and divide it:
+		-	[1/3, 7/18] for 'k'
+		-	[7/18, 11/18] for 'i'
+		-	[11/18, 1/2] for 'w'
+	-	The fraction 33/162, now scaled to this new interval, falls in
+		[11/18, 1/2], so the third character is 'w'.
+
+	4.	Fourth Character:
+	-	Update the interval to [11/18, 1/2] and divide it:
+		-	[11/18, 13/24] for 'k'
+		-	[13/24, 17/24] for 'i'
+		-	[17/24, 1/2] for 'w'
+	-	The fraction 33/162, scaled to this interval, falls in [13/24, 17/24],
+		so the fourth character is 'i'.
+
+	So, by repeating this process for each character and updating the intervals
+	accordingly, we decode 33/162 back into the original message "KIWI".
+	"""
+	lower_bound = Fraction(0, 1)
+	upper_bound = Fraction(1, 1)
+	decoded_message = []
+	for _ in range(message_length):
+		range_width = upper_bound - lower_bound
+		for symbol, (symbol_lower, symbol_upper) in symbol_probabilities.items():
+			if lower_bound <= encoded_number < lower_bound + range_width * symbol_upper:
+				decoded_message.append(symbol)
+				upper_bound = lower_bound + range_width * symbol_upper
+				lower_bound = lower_bound + range_width * symbol_lower
+				break
+	return decoded_message
+
+
+if __name__ == '__main__':
+	def arithmetic_encode_and_verify(message, symbol_probabilities):
+		encoded_value, logarithm, encoding_process = arithmetic_encode(message, symbol_probabilities)
+		decoded_message = arithmetic_decode(encoded_value, symbol_probabilities, len(message))
+		is_correct = decoded_message == list(message)
+
+		return encoded_value, logarithm, is_correct, encoding_process
+
+	message = 'kiwikiwikiwikiwikiwiiiiwwwwkikikiikwikwikw'
+	# message = 'kiwi' # Docstring example 1
+	probabilities = {'k': (0, Fraction(1, 3)), 'i': (Fraction(1, 3), Fraction(2, 3)), 'w': (Fraction(2, 3), 1)}
+	print(probabilities)
+	encoded_value, logarithm, is_correct, encoding_process = arithmetic_encode_and_verify(message, probabilities)
+	# print(f"Process: {encoding_process}") # Print here
+	print(f"Encoded Value: {encoded_value}")
+	print(f"logarithm of the denominator: {logarithm}")
+	print(f"Decoding Correct: {is_correct}")
+
+	probabilities = count_distribute_probability(count_frequency(message))
+	print(probabilities)
+	encoded_value, logarithm, is_correct, encoding_process = arithmetic_encode_and_verify(message, probabilities)
+	print(f"Encoded Value: {encoded_value}")
+	print(f"logarithm of the denominator: {logarithm}")
+	print(f"Decoding Correct: {is_correct}")
+
+	probabilities = {'k': (0, Fraction(2, 8)), 'i': (Fraction(2, 8), Fraction(5, 8)), 'w': (Fraction(5, 8), 1)}
+	print(probabilities)
+	encoded_value, logarithm, is_correct, encoding_process = arithmetic_encode_and_verify(message, probabilities)
+	print(f"Encoded Value: {encoded_value}")
+	print(f"logarithm of the denominator: {logarithm}")
+	print(f"Decoding Correct: {is_correct}")
+
+	probabilities = {'k': (0, Fraction(1, 4)), 'i': (Fraction(1, 4), Fraction(3, 4)), 'w': (Fraction(3, 4), 1)}
+	print(probabilities)
+	encoded_value, logarithm, is_correct, encoding_process = arithmetic_encode_and_verify(message, probabilities)
+	print(f"Encoded Value: {encoded_value}")
+	print(f"logarithm of the denominator: {logarithm}")
+	print(f"Decoding Correct: {is_correct}")
